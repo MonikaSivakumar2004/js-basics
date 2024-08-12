0e1585c379cc8ad9dabfc7487249abdf21ae699a
MONIKA <monikasivakumar240@gmail.com>
2024-08-12 09:59:42 -0700

    Create arrary.js

---
+++ arrary.js
@@ -0,0 +1,9 @@
+const arr = [210, 18, 113, 45, 154];
+if (arr.includes(121)) 
+{
+    console.log("The num is present in the array.");
+} 
+else
+{
+    console.log("The num is not present in the array.");
+}
