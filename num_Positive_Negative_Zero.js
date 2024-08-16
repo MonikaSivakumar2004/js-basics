ee5c9a0f696537462a81c45dd2ae667d56641b67
MONIKA <monikasivakumar240@gmail.com>
2024-08-16 21:27:09 +0530

    Create num_Positive_Negative_Zero.js

---
+++ num_Positive_Negative_Zero.js
@@ -0,0 +1,13 @@
+const number = 5; 
+if (number > 0) 
+{
+    console.log("The number is positive");
+} 
+else if (number < 0)
+{
+    console.log("The number is negative");
+} 
+else
+{
+    console.log("The number is zero");
+}
