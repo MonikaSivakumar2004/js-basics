4a242d564877f04c443e1c942737183c259236ae
MONIKA <monikasivakumar240@gmail.com>
2024-08-23 23:41:25 +0530

    Create sumOfOddNumber.js

---
+++ sumOfOddNumber.js
@@ -0,0 +1,13 @@
+function sumOfOddNumbers(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const oddSum = sumOfOddNumbers(numbers);
+console.log("Sum of odd numbers:", oddSum);
